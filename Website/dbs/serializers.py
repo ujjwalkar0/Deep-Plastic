b79c9e7502df5d502572a0806fc26dc224ead620
Ujjwal Kar <ujjwalkar21@gmail.com>
2022-10-05 16:58:49 +0530

    2.0

--- Website/dbs/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import UploadImageTest
-from versatileimagefield.serializers import VersatileImageFieldSerializer
-
-
-class ImageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = UploadImageTest
-        fields = ('__all__')
